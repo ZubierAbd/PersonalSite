ddfcd5f7f65c4d131e685cd8dcbbdeb739aa81ef
Zubier <zubierabd@gmail.com>
2020-09-23 23:12:02 -0400

    made minor changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,8 @@ import React from "react"
 import Layout from "../components/layout"
 import TechCard from "../components/techCard"
 import { DiJavascript1 } from "react-icons/di"
-import { FaHtml5, FaCss3Alt, FaJava, FaGit, FaReact, FaAngular } from 'react-icons/fa'
+import { FaHtml5, FaCss3Alt, FaJava, FaGit, FaReact, FaAngular, FaDocker, FaAws } from 'react-icons/fa'
+import { SiMongodb } from 'react-icons/si'
 import SEO from "../components/seo"
 
 
@@ -18,29 +19,43 @@ const AboutPage = () => {
         <h2>About Me</h2>
 
         <div >
-          <p style={{}}>
+          <p style={{ width: "80vw" }}>
             I am a FullStack Developer living in Toronto and currently work for one of the largest Banks in Canada in Capital Markets. I am equally at home working both front end and back and switch depending on the role.             I am also an internationally published short story author, hailing
           from Bangladesh{" "}
           </p>
 
           <p>
-            <h4>Technologies I know</h4>
+            <h3>Technologies I know</h3>
             <ul className="tech-container">
-              <span className="techy"><DiJavascript1 /><span> </span></span>
-              <span className="techy"><FaHtml5 /><span> </span></span>
-              <span className="techy"><FaCss3Alt /><span> </span></span>
-              <span className="techy"><FaJava /><span> </span></span>
-              <span className="techy"><FaGit /><span> </span></span>
-              <span className="techy"><FaReact /><span> </span></span>
-              <span className="techy"><FaAngular /><span> </span></span>
+              <Tech icon={<DiJavascript1 />} name="Javascript" />
+              <Tech icon={<FaHtml5 />} name="HTML" />
+              <Tech icon={<FaCss3Alt />} name="CSS" />
+              <Tech icon={<FaJava />} name="Java" />
+              <Tech icon={<FaGit />} name="GIT" />
+              <Tech icon={<FaReact />} name="React" />
+              <Tech icon={<FaAngular />} name="Angular" />
             </ul>
           </p></div>
 
         <p>
-          <h4>Technologies I am learning or planning to learn</h4>
+          <h3>Technologies I am learning or planning to learn</h3>
+          <ul className="tech-container">
+            <Tech icon={<FaDocker />} name="Docker" />
+            <Tech icon={<FaAws />} name="AWS" />
+            <Tech icon={<SiMongodb />} name="MongoDb" />
+          </ul>
         </p>
       </div>
     </Layout>
+  )
+}
+
+const Tech = ({ icon, name }) => {
+  return (
+    <div className="TechCard">
+      <p className="techy">{icon}</p>
+      <p >{name}</p>
+    </div>
   )
 }
 
